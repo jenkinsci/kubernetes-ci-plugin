7eee92c1a06c3544631dcd2523c7b38eaa46e61d
Guillermo Sánchez Uríen <guillermo@elasticbox.com>
2017-02-21 15:35:21 +0100

    Add canProvision tests for KubernetesCloud

--- src/test/java/com/elasticbox/jenkins/k8s/plugin/TestBaseKubernetes.java
+++ src/test/java/com/elasticbox/jenkins/k8s/plugin/TestBaseKubernetes.java
@@ -10,21 +10,12 @@ package com.elasticbox.jenkins.k8s.plugin;
 
 import com.elasticbox.jenkins.k8s.plugin.clouds.ChartRepositoryConfig;
 import com.elasticbox.jenkins.k8s.plugin.clouds.KubernetesCloud;
-import com.elasticbox.jenkins.k8s.plugin.clouds.KubernetesCloudParams;
-import com.elasticbox.jenkins.k8s.repositories.KubernetesRepository;
-import com.elasticbox.jenkins.k8s.repositories.error.RepositoryException;
+import com.elasticbox.jenkins.k8s.plugin.clouds.PodSlaveConfig;
 import org.junit.Before;
 import org.junit.Rule;
 import org.jvnet.hudson.test.JenkinsRule;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 
-import java.util.Arrays;
 import java.util.Collections;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
 
 public class TestBaseKubernetes {
 
@@ -32,7 +23,6 @@ public class TestBaseKubernetes {
     protected static final String FAKE_URL = "http://fake.url";
     protected static final String FAKE_NS = "fakeNs";
     protected static final String FAKE_CHARTS_REPO = "FakeChartsRepo";
-    protected static final String FAKE_KUBE_CLOUD = "FakeKubeCloudName";
     protected static final String FAKE_MOCK_EXCEPTION = "FakeMockException";
 
     @Rule
@@ -44,8 +34,10 @@ public class TestBaseKubernetes {
     @Before
     public void setupCloud() throws Exception {
         fakeChartRepoCfg = new ChartRepositoryConfig(FAKE_CHARTS_REPO, FAKE_URL, EMPTY);
+        PodSlaveConfig fakePodSlaveConfig = new PodSlaveConfig("fakeID", "fakePodSlaveConfig", EMPTY, EMPTY);
+
         cloud = new KubernetesCloud(EMPTY, "FakeKubeCloud", FAKE_URL, FAKE_NS, "10", EMPTY, EMPTY,
-                                    Collections.singletonList(fakeChartRepoCfg), null);
+                                    Collections.singletonList(fakeChartRepoCfg), Collections.singletonList(fakePodSlaveConfig));
 
         jenkins.getInstance().clouds.add(cloud);
     }
